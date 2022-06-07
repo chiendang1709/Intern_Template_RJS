bac7f7032d929c2ca7ec58fea646081903c88232
Chiến <chiendang1011@gmail.com>
2022-06-07 12:29:35 +0700

    7/6/2022

---
+++ src/routes/route.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const route = () => {
+  return (
+    <div>route</div>
+  )
+}
+
+export default route
+//Thư mục này để chứa file config tất cả router của app. Thay vì config trực tiếp ở file root.js thì chúng ta define sẵn ở đây, để dễ nhìn và có gì thay đổi cũng tìm nhanh tiện hơn.
